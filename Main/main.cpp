d4cff66223b2dde9270a6383d0dd307aec10d7d5
gstirlingAC <gordon.stirling@ayrshire.ac.uk>
2016-08-05 09:15:56 +0100

    In this tutorial we refactor the code to create a more resuable structure.

--- Main/main.cpp
+++ Main/main.cpp
@@ -2,16 +2,17 @@
 
 SDL_Window* g_pWindow = 0;
 SDL_Renderer* g_pRenderer = 0;
+bool g_bRunning = false; // this will create a loop
 
-int main(int argc, char* args[])
+bool init(const char* title, int xpos, int ypos, int height,
+	int width, int flags)
 {
 	// initialise SDL
 	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
 	{
 		// if succeeded create our window
-		g_pWindow = SDL_CreateWindow("Tutorial 1: Setting up SDL",
-			SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-			640, 480, SDL_WINDOW_SHOWN);
+		g_pWindow = SDL_CreateWindow(title, xpos, ypos, height,
+			width, flags);
 
 		// if the window creation succeeded, create our renderer
 		if (g_pWindow != 0)
@@ -21,11 +22,14 @@ int main(int argc, char* args[])
 	}
 	else
 	{
-		return 1; // SDL could not initialise
+		return false; // SDL could not initialise
 	}
 
-	// if the above checks succeed, draw the window
+	return true;
+}
 
+void render()
+{
 	// set to black, this function expects red, green, blue and
 	// alpha as colour values
 	SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
@@ -35,9 +39,25 @@ int main(int argc, char* args[])
 
 	// show the window
 	SDL_RenderPresent(g_pRenderer);
+}
 
-	// set a delay before quitting
-	SDL_Delay(5000);
+int main(int argc, char* args[])
+{
+	if (init("Tutorial 2: Thinking reusability",
+		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640,
+		480, SDL_WINDOW_SHOWN))
+	{
+		g_bRunning = true;
+	}
+	else
+	{
+		return 1; // something's wrong
+	}
+
+	while (g_bRunning)
+	{
+		render();
+	}
 
 	// clean up SDL
 	SDL_Quit();
